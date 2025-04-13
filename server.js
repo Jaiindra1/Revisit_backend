f0569a53cdc1ff40e8811a8bec93106f6754ec8f
Jai indra <168549352+Jaiindra1@users.noreply.github.com>
2025-04-13 20:24:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
+
 app.use('/api/categories', require('./routes/categoryRoutes'));
 
 const PORT = process.env.PORT || 5000;
